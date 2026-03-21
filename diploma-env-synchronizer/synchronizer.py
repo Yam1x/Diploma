a21946af7645c71f9b22d332c43f35ef709aa4ed
Yam1x <rychkov03@yandex.ru>
2026-03-21 11:54:52 +0000

    Fix env-synchronizer Helmfile path

--- diploma-env-synchronizer/synchronizer.py
+++ diploma-env-synchronizer/synchronizer.py
@@ -1,14 +1,29 @@
-import subprocess
 import os
+import subprocess
+from pathlib import Path
 
-if (os.getenv("SYNCHRONIZER_ENABLED") == "true"):
+
+if os.getenv("SYNCHRONIZER_ENABLED") == "true":
     # By default subprocess.run doesn't throw error if the command was failed due runtime and pod completes without any errors
     # with check = True subprocess.run will throw error and pod will be completed with error
-    subprocess.run(['git', 'clone', f'https://github.com/{os.getenv("ENV_REPOSITORY")}.git'], check = True)
+    repository = os.getenv("ENV_REPOSITORY", "")
+    repository_dir = Path(repository.rsplit("/", 1)[-1])
+    helmfile_path = repository_dir / os.getenv("PATH_TO_HELMFILE", "")
 
-    subprocess.run(['helmfile', 'cache', 'cleanup'], check = True)
-
-    subprocess.run(['helmfile', '--environment', f'{os.getenv("NAMESPACE")}', '--namespace', f'{os.getenv("NAMESPACE")}', '-f', f'{os.getenv("PATH_TO_HELMFILE")}', 'apply'], check = True)
-
+    subprocess.run(["git", "clone", f"https://github.com/{repository}.git"], check=True)
+    subprocess.run(["helmfile", "cache", "cleanup"], check=True)
+    subprocess.run(
+        [
+            "helmfile",
+            "--environment",
+            f'{os.getenv("NAMESPACE")}',
+            "--namespace",
+            f'{os.getenv("NAMESPACE")}',
+            "-f",
+            str(helmfile_path),
+            "apply",
+        ],
+        check=True,
+    )
 else:
     print("SYNCHRONIZER IS DISABLED")
